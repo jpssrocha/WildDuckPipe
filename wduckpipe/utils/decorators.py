2f379a43ef149f5af261cfa1baddac4c253700d9
jpssrocha <joaopedro0498@hotmail.com>
2020-07-01 16:25:42 -0300

    Added skelecton for future functionalities

--- wduckpipe/utils/decorators.py
+++ wduckpipe/utils/decorators.py
@@ -1,8 +1,32 @@
 """Generic extra functionality for functions"""
 
-def check_disk():
+def check_disk(files):
     """
     Decorator to wrap functions that require writing. Check space on disk
+    given estimate of file size.
+    
+    Args:
+        files -- List of files to estimate size.
+    """
+    # Estimate size of files to be writen
+
+    # Check available space on disk
+
+    # Raise error if there is no space left
+
+    return None
+
+def timeit(function):
+    """
+    Decorator to benchmark functions
     """
 
+    # import sys timer
+
+    return None
+
+def get_from_ds9(window):
+    """
+    Use ds9 socket to get selection matrice
+    """
     return None
---
+++ wduckpipe/utils/graph.py
@@ -0,0 +1,9 @@
+"""Customized plotting functions
+
+"""
+
+def imexam_fwhm(star_block):
+    """
+    Iraf style fwhm plot
+    """
+    return None
